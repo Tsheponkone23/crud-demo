18e4f71d6e649de293c51b9337da113fc8ca8e89
Tshepo Nkone <tshepo.nkone@standardbank.co.za>
2021-03-05 09:31:09 +0200

    Adding my project structure - Adding controller class

---
+++ src/main/java/com/cruddemo/cruddemo/controller/CrudController.java
@@ -0,0 +1,9 @@
+package com.cruddemo.cruddemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/customer")
+public class CrudController {
+}
